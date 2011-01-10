3af48378c9689930d0c2f1837c512192139f449a
Olya Lobodina <relloudder@gmail.com>
2011-01-10 22:26:38 +1000

    Add blocks and change canvas's background

--- libraryDraw.js
+++ libraryDraw.js
@@ -1,15 +1,15 @@
 var DrawingHelpers = (function() {
 
     colorBall = function (ctx,x0,y0,r,col){
-	    var gradient = ctx.createRadialGradient(x0+r/4,y0-r*1/3,r/15,x0,y0,r);
- 	    gradient.addColorStop(0,'#ffffff');
-  	    gradient.addColorStop(0.85,col);
-  	    ctx.fillStyle = gradient;
-  	    ctx.strokeStyle = col;
+        var gradient = ctx.createRadialGradient(x0+r/4,y0-r*1/3,r/15,x0,y0,r);
+        gradient.addColorStop(0,'#ffffff');
+        gradient.addColorStop(0.85,col);
+        ctx.fillStyle = gradient;
+        ctx.strokeStyle = col;
     }
 
     return {
-	    colorBall : colorBall
+        colorBall : colorBall
     };
 
 })();
@@ -18,29 +18,29 @@ var DrawingHelpers = (function() {
 var DrawForVis = (function(){
 
     back = function(ctx,col1,col2,w,h){
-	    ctx.save();
+        ctx.save();
         var g = ctx.createLinearGradient(0,0,0,h);
         g.addColorStop(0,col1);
         g.addColorStop(1,col2);
         ctx.fillStyle = g;
         ctx.fillRect(0,0,w,h);
-	    ctx.restore();
+        ctx.restore();
     }
 
     ball = function(ctx,x0,y0,r,col){
         ctx.save();
-	    ctx.beginPath();
-	    DrawingHelpers.colorBall(ctx,x0,y0,r,col); //создаем градиент для закраски шарика
-	    ctx.arc(x0,y0,r,0,2*Math.PI,false); //рисуем шарик
-	    ctx.fill();
-	    ctx.closePath();
-	    ctx.restore();
+        ctx.beginPath();
+        DrawingHelpers.colorBall(ctx,x0,y0,r,col); //создаем градиент для закраски шарика
+        ctx.arc(x0,y0,r,0,2*Math.PI,false); //рисуем шарик
+        ctx.fill();
+        ctx.closePath();
+        ctx.restore();
     }
 
     halfBall = function(ctx,x0,y0,r,col){
-	    ctx.save();	
-  	    DrawingHelpers.colorBall(ctx,x0,y0,r,col);
-	    ctx.beginPath(); //передняя стенка
+        ctx.save();
+        DrawingHelpers.colorBall(ctx,x0,y0,r,col);
+        ctx.beginPath(); //передняя стенка
         ctx.arc(x0,y0,r,0,Math.PI,false);
         ctx.fill();
         ctx.closePath();
@@ -55,7 +55,7 @@ var DrawForVis = (function(){
     }
 
     alphaBall = function(ctx,x0,y0,r,col,alpha){
-	    ctx.save();
+        ctx.save();
         DrawingHelpers.colorBall(ctx,x0,y0,r,col);
         with (Math) {
             ctx.beginPath();
@@ -67,11 +67,11 @@ var DrawForVis = (function(){
             var dy = y0 + sin(4/3*alpha*PI/180)*r/2;
             ctx.moveTo(x1,y1);
             ctx.quadraticCurveTo(dx,dy,x0-0.1*r,y0-0.1*r);
-	        var x = x0 + cos(-(alpha+180)*PI/180)*r;
-	        var y = y0 + sin(-(alpha+180)*PI/180)*r;
-	        var dx = x0 + cos(-(2/3*alpha+180)*PI/180)*r/2;
-	        var dy = y0 + sin(-(2/3*alpha+180)*PI/180)*r/2;
-	        ctx.quadraticCurveTo(dx,dy,x,y);
+            var x = x0 + cos(-(alpha+180)*PI/180)*r;
+            var y = y0 + sin(-(alpha+180)*PI/180)*r;
+            var dx = x0 + cos(-(2/3*alpha+180)*PI/180)*r/2;
+            var dy = y0 + sin(-(2/3*alpha+180)*PI/180)*r/2;
+            ctx.quadraticCurveTo(dx,dy,x,y);
             ctx.stroke();
             ctx.fillStyle = "#ffffff";
             ctx.quadraticCurveTo(x0+r/2,y0+r,x1,y1);
@@ -96,7 +96,7 @@ var DrawForVis = (function(){
             ctx.closePath();
             ctx.closePath();
         }
-	    ctx.restore();
+        ctx.restore();
     }
 
     return {
