05902f46de238864fcaaf367f6b91f3d9ac1aec4
Olya Lobodina <relloudder@gmail.com>
2011-01-14 18:45:10 +1000

    Paint balls by clicking on the icon.
    Add jquery library and main.js

---
+++ js/main.js
@@ -0,0 +1,38 @@
+var ctx;
+
+$(document).ready(function(){
+    initAplication();
+    runInterface();
+});
+
+function runInterface(){
+    $('#play').click(function(){
+        var r = 12;
+        var y = 50;
+        var col = "#ff0000";
+        for (var j = 1; j<=3; j++){
+            DrawForVis.ball(ctx,50,y,r,col);
+            var i = -85;
+            var x = 100;
+            while(i < 0){
+                DrawForVis.alphaBall(ctx,x,y,r,col,i);
+                x+=50;
+                i+=10;
+            }
+            DrawForVis.halfBall(ctx,x,y,r,col);
+            r*=1.25;
+            y+=50;
+        } 
+    });
+}
+
+function initAplication(){
+    var canvas;
+    canvas = document.getElementById("firstStep");
+    if(canvas.getContext){
+        ctx = canvas.getContext("2d");
+        canvas.width = document.body.offsetWidth*2/3;
+        canvas.height = document.body.offsetHeight/10*9;
+        DrawForVis.back(ctx,"#7cb7e3","#cccccc",canvas.width,canvas.height);
+    }
+}
