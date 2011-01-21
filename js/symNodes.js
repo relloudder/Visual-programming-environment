11f67020446c9cb98c045c9f19533b8c7ec722f9
Olya Lobodina <relloudder@gmail.com>
2011-01-21 15:48:21 +1000

    Rewrite symNodes using mootools

--- js/symNodes.js
+++ js/symNodes.js
@@ -1,105 +1,73 @@
-function extend (Child,Parent){
-    var F = function(){}
-    F.prototype = Parent.prototype;
-    Child.prototype = new F();
-    Child.prototype.constructor = Child;
-    Child.superclass = Parent.prototype;
-}
-
-//Symbol
-Symbol = function(pX, pY){
-    with(this){
-        posX = pX;
-        posY = pY;
-        visible = true;
-        transp = 1; //прозрачность
+Symbol = new Class({
+    initialize: function(pX,pY){
+        this.posX = pX;
+        this.posY = pY;
+    },
+    visible : true,
+    transp : 1,
+	draw : function(ctx){
+	},
+ 	setVisible : function(vis){
+        this.visible = vis;
+    },
+    getVisible : function(){
+        return this.visible;
+    },
+    getPosX : function(){
+        return this.posX;
+    },
+    getPosY : function(){
+        return this.posY;
     }
-}
+});
 
-Symbol.prototype.posX = null;
-Symbol.prototype.posY = null;
-Symbol.prototype.visible = null;
-Symbol.prototype.transp = null;
-
-Symbol.prototype.draw = function(ctx){};
-
-Symbol.prototype.setVisible = function(vis){
-    this.visible = vis;
-}
-
-Symbol.prototype.getVisible = function(){
-    return this.visible;
-}
-
-Symbol.prototype.getPosX = function(){
-    return this.posX;
-}
-
-Symbol.prototype.getPosY = function(){
-    return this.posY;
-}
-
-//SymVar
-SymVar = function (v, pX, pY, cVar){
-    SymVar.superclass.constructor.apply(this,[pX,pY]);
-	with (this){
-        val = v;
-        colVar = cVar;
-        rVar = 20;
+SymVar = new Class({
+    Extends: Symbol,
+    initialize: function (v,pX,pY,cVar,cR){
+	    with(this){
+            parent(pX,pY);
+			val = v;
+            colVar = cVar;
+            rVar = cR;
+       }
+    },
+    getRVar : function(){
+       return this.rVar;
+    },
+    setRVar : function(r){
+        this.rVar = r;
+    },
+    getValue : function(){
+        return this.val;
+    },
+    setValue : function(v){
+        this.val = v;
+    },
+    getColVar : function(){
+        return this.colVar;
+    },
+	draw : function(ctx){
+        with(this){
+            DrawForVis(ctx).ball(posX,posY,rVar,colVar);
+            if (val != '') DrawForVis(ctx).text(val,posX,posY,rVar,s0);
+        }
     }
-}
+});
 
-extend(SymVar,Symbol);
-
-SymVar.prototype.val = null;
-SymVar.prototype.rVar = null;
-SymVar.prototype.colVar = null;
-
-SymVar.prototype.getRVar = function(){
-    return this.rVar;
-}
-
-SymVar.prototype.setRVar = function(r){
-    this.rVar = r; 
-}
-
-SymVar.prototype.getValue = function(){
-    return this.val;
-}
-
-SymVar.prototype.setValue = function(v){
-    this.val = v;
-}
-
-SymVar.prototype.getColVar = function(){
-    return this.colVar;
-}
-
-SymVar.prototype.draw = function(ctx){
-    with(this){
-        DrawForVis(ctx).ball(posX,posY,rVar,colVar);
-        if (val != '') DrawForVis(ctx).text(val,posX,posY,rVar,0);
+SymVarName = new Class({
+    Extends: SymVar,
+    initialize: function (v,pX,pY,cVar,cR,nameV){
+        this.parent(v,pX,pY,cVar,cR);
+        this.name = nameV;
+    },
+    getName : function (){
+        return this.name;
+    },
+    draw : function(ctx){
+        with(this){
+            DrawForVis(ctx).ball(posX,posY,rVar,colVar);
+            DrawForVis(ctx).text(val,posX,posY,rVar,0);
+            DrawForVis(ctx).flag(posX-rVar,posY-4,4,6,colVar,name,rVar,155*Math.PI/180);
+        }
     }
-}
-
-//SymVarName
-SymVarName = function (v,pX,pY,cVar,nameV){
-    SymVarName.superclass.constructor.apply(this,[v,pX,pY,cVar]);
-    this.name = nameV;
-}
-
-extend(SymVarName,SymVar);
-
-SymVarName.prototype.name = null;
-
-SymVarName.prototype.getName = function (){
-    return this.name;
-}
-
-SymVarName.prototype.draw = function(ctx){
-    with(this){
-        DrawForVis(ctx).ball(posX,posY,rVar,colVar);
-        DrawForVis(ctx).text(val,posX,posY,rVar,0);
-        DrawForVis(ctx).flag(posX-rVar,posY-4,4,6,colVar,name,rVar,155*Math.PI/180);
-    }
-}
+})
