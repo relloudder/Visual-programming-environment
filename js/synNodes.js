0ab89eac5faf7c494ef068a24e231a9bada71b34
Olya Lobodina <relloudder@gmail.com>
2011-03-31 19:48:36 +1100

    Add SynVar, SynArray, SynRecord

--- js/synNodes.js
+++ js/synNodes.js
@@ -9,3 +9,92 @@ SynExpr = new Class({
         return this.type;
     }
 });
+
+SynVar = new Class({
+    Extends: SynExpr,
+    initialize: function(symVar) {
+        this.symVar = symVar;
+        this.type = symVar.type;
+    },
+    symVar: null,
+    getValue: function() {
+        return this.symVar.getValue();
+    },
+    setValue: function(value) {
+        this.symVar.setValue(value);
+    },
+    getSymbol: function() {
+        return this.symVar;
+    },
+    getName: function() {
+        return this.symVar.name;
+    }
+});
+
+SynArray = new Class({
+    Extends: SynExpr,
+    initialize: function(left,right,symbol) {
+        this.left = left;
+        this.right = right;
+        this.symbolArray = symbol;
+    },
+    left: null, //link for synArray
+    right: null, //expression for index
+    symbolArray: null, //type of array's items
+    getValue: function() {
+        return this.getSymbol().getValue();
+    },
+    getName: function() {
+        return this.left.name;
+    },
+    getType: function() {
+        return this.symbolArray.getType();
+    },
+    getSymbol: function() { //returns sym from treeVar
+        var result = this.right.operation(false);
+        var item = this.left.getItemArrByNum(result);
+        if((this.symbolArray instanceof SynArray)||(this.symbolArray instanceof SynRecord)) {
+            this.symbolArray.setItemLeft(item);
+            return this.symbolArr.getSymbol();
+        }
+        return item;
+    },
+    setItemLeft: function(item) {
+        this.left = item;
+    }
+});
+
+SynRecord = new Class({
+    Extends: SynExpr,
+    initialize: function(left,right) {
+        this.left = left; //link for synRecord
+        this.right = right; //record's fields
+    },
+    left: null,
+    right: null,
+    getValue: function() {
+        return this.getSymbol().getValue();
+    },
+    getType: function() {
+        return this.right.getType();
+    },
+    getSymbol: function() {
+        var item = this;
+        while(item.right instanceof SynRecord)
+	        item  =  item.right;
+	    var symb;
+	    if(item.right instanceof SynArray)
+            symb = item.right.getSymbol();
+	    else symb = item.left.getItemByName(item.right.getName());
+	    return symb;
+    },
+    getName: function() {
+        return this.left.name;
+    },
+    setItemLeft = function(item) {
+        this.left = item;
+        if (this instanceof SynRecord)
+            this.right.left = item.getItemByName(this.right.getName());
+    }
+});
+
