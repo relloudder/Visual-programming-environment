b1badb08ebb4b604ff45f08e427806833b3607e7
Olya Lobodina <relloudder@gmail.com>
2011-03-31 19:08:32 +1100

    Create synNodes.js
    Add class SynExpr

---
+++ js/synNodes.js
@@ -0,0 +1,11 @@
+SynExpr = new Class({
+    initialize: function() {},
+    type: '',
+    getValue: function() {
+        return 0;
+    },
+    setValue: function(value) {},
+    getType : function() {
+        return this.type;
+    }
+});
